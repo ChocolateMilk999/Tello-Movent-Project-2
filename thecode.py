3beb8b6482893b3fd1cbd8cb1a24bb5d5a059725
ChocolateMilk999 <92953891+ChocolateMilk999@users.noreply.github.com>
2021-10-28 13:28:48 -0700

    Update thecode.py
    Updated with the sleep command so the code actually works.

--- thecode.py
+++ thecode.py
@@ -1,17 +1,33 @@
 from djitellopy import Tello
+from time import sleep
 
 tello = Tello()
+
 
 tello.connect()
 tello.take_off()
 
+
+sleep(2)
 tello.move_up(106)
 
+
+sleep(2)
 tello.move_forward(304)
+
+sleep(2)
 tello.move_left(304)
+
+sleep(2)
 tello.move_backwards(304)
+
+sleep(2)
 tello.move_right(304)
 
+
+sleep(2)
 tello.flip_back()
 
+
+sleep(2)
 tello.land()
